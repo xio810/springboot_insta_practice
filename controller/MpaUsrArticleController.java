e25523796e702cd91e099eaf05416b7521a21043
XIO <69510098+xio810@users.noreply.github.com>
2021-07-08 03:36:11 +0900

    게시물 삭제 기능

--- controller/MpaUsrArticleController.java
+++ controller/MpaUsrArticleController.java
@@ -14,13 +14,15 @@ import com.sbs.untact.util.Util;
 @Controller
 public class MpaUsrArticleController {
 	private List<Article> articles;
-	private int articleLastId = 0;
+	private int articleLastId;
 
 	public MpaUsrArticleController() {
 		articles = new ArrayList<>();
 		articleLastId = 0;
+		makeTestData();
 	}
-
+	
+	//글쓰기 
 	@RequestMapping("/mpaUsr/article/doWrite")
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
@@ -29,33 +31,66 @@ public class MpaUsrArticleController {
 
 		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
+	
+	//삭제 
+	@RequestMapping("/mpaUsr/article/doDelete")
+	@ResponseBody
+	public ResultData doDelete(int id) {
+		boolean deleted = deleteArticleById(id);
+		
+		if (deleted == false) {
+			return new ResultData("S-1", id + "번 글이 존재하지 않습니다.", "id",id);			
+		}
+		
+		return new ResultData("S-1", id + "번 글이 삭제되었습니다.", "id",id);
 
-	private int writeArticle(String title, String body) {
-
-		int id = articleLastId + 1;
-		String regDate = Util.getNowDateStr();
-		String updateDate = Util.getNowDateStr();
-
-		Article article = new Article(id, regDate, updateDate, title, body);
-		articles.add(article);
-
-		articleLastId = id;
-
-		return 0;
 	}
 
+
+	//글 반환 
 	@RequestMapping("/mpaUsr/article/getArticle")
 	@ResponseBody
 	public ResultData getArticle(int id) {
 		Article article = getArticleById(id);
 
 		if (article == null) {
-			return new ResultData("F-1", id + "번 글은 없는 글 입니다.", "id", id);
+			return new ResultData("F-1", id + "번 글은 존재하지 않습니다.", "id", id);
 		}
 
 		return new ResultData("S-1", article.getId() + "번 글 입니다.", "article", article);
 	}
-
+	
+	////////////////////////내부//////////////////////// 
+	private void makeTestData() {
+		for ( int i = 0; i < 3; i++ ) {
+			writeArticle("제목1", "내용1");			
+		}
+	}
+	
+	private boolean deleteArticleById(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				articles.remove(article);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private int writeArticle(String title, String body) {
+		int id = articleLastId + 1;
+		String regDate = Util.getNowDateStr();
+		String updateDate = Util.getNowDateStr();
+		
+		Article article = new Article(id, regDate, updateDate, title, body);
+		articles.add(article);
+		
+		articleLastId = id;
+		
+		return id;
+	}
+	
+	
 	private Article getArticleById(int id) {
 		for (Article article : articles) {
 			if (article.getId() == id) {
@@ -63,15 +98,13 @@ public class MpaUsrArticleController {
 			}
 		}
 
-//		for (int i = 0; i < articles.size(); i++) {
-//			Article article = articles.get(i);
-//			
-//			if (article.getId() == id) {
-//				return article;
-//			}
-//		}
+		/*
+		 * for ( int i = 0; i < articles.size(); i++ ) { Article article =
+		 * articles.get(i);
+		 * 
+		 * if ( article.getId() == id ) { return article; } }
+		 */
 
 		return null;
 	}
-
 }// mpaUsrArticleController - end
