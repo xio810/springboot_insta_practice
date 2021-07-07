45cedc07d306d6eaa92a182be8f148c9b8a25fa6
XIO <69510098+xio810@users.noreply.github.com>
2021-07-07 21:37:53 +0900

    ResultData를 범용화

--- untact/controller/MpaUsrArticleController.java
+++ untact/controller/MpaUsrArticleController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sbs.untact.dto.Article;
+import com.sbs.untact.dto.ResultData;
 import com.sbs.untact.util.Util;
 
 @Controller
@@ -13,7 +15,7 @@ public class MpaUsrArticleController {
 	
 	@RequestMapping("/mpaUsr/article/doWrite")
 	@ResponseBody
-	public Article doWrite(String title, String body) {
+	public ResultData doWrite(String title, String body) {
 		int id = articleLastId + 1;
 		
 		String regDate = Util.getNowDateStr();
@@ -23,68 +25,9 @@ public class MpaUsrArticleController {
 		
 		articleLastId = id;
 		
-		return article;
+		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
 	
 }//mpaUsrArticleController - end
 
-class Article {
-	private int id;
-	private String regDate;
-	private String updateDate;
-	private String title;
-	private String body;
-	
-	
-	
-	//Constructor
-	public Article(int id, String regDate, String updateDate, String title, String body) {
-		super();
-		this.id = id;
-		this.regDate = regDate;
-		this.updateDate = updateDate;
-		this.title = title;
-		this.body = body;
-	}
-	
-	//toString
-	@Override
-	public String toString() {
-		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
-				+ ", body=" + body + "]";
-	}
-	
-	//getter and setter
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(String regDate) {
-		this.regDate = regDate;
-	}
-	public String getUpdateDate() {
-		return updateDate;
-	}
-	public void setUpdateDate(String updateDate) {
-		this.updateDate = updateDate;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getBody() {
-		return body;
-	}
-	public void setBody(String body) {
-		this.body = body;
-	}
-	
-	
-}
+
