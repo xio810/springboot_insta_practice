0ab30652d3cf4bbdf09b2d643df6b15e54fec706
XIO <69510098+xio810@users.noreply.github.com>
2021-07-09 21:49:27 +0900

    jsp 연결

--- insta/src/main/java/com/sbs/untact/controller/MpaUsrHomeController.java
+++ insta/src/main/java/com/sbs/untact/controller/MpaUsrHomeController.java
@@ -1,5 +1,10 @@
 package com.sbs.untact.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,20 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MpaUsrHomeController {
 	@RequestMapping("/mpaUsr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕";
-	}
-	
-	@RequestMapping("/mpaUsr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "bye";
-	}
-	
-	@RequestMapping("/mpaUsr/home/main3")
-	@ResponseBody
-	public int showMain3(int a, int b) {
-		return a-b;
+		return "mpaUsr/home/main";
 	}
 }
