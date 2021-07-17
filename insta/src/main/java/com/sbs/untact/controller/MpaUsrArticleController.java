4804f0f4e11194adefd8dfcf46e0191dd51fa216
XIO <69510098+xio810@users.noreply.github.com>
2021-07-18 04:18:16 +0900

    공통 에러 처리 페이지, redirect.jsp 추가

--- insta/src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
+++ insta/src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
@@ -79,7 +79,8 @@ public class MpaUsrArticleController {
 		Board board = articleService.getBoardById(boardId);
 				
 		if ( board == null ) {
-			return "존재하지 않는 게시판 입니다.";
+			req.setAttribute("msg", boardId + "번은 존재하지 않는 게시판 입니다.");
+			return "common/redirect";
 		}
 		
 		req.setAttribute("board", board);
